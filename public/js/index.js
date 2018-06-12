5015e9d015bc6a5abd9d2c60d0d7c01046d73060
Wayne Bruton <waynebruton@icloud.com>
2018-06-12 13:29:48 +0200

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
 
@@ -22,7 +38,8 @@ socket.on('newMessage', function(message){
         createdAt: formattedTime
     });
 
-    jQuery('#messages').append(html);    
+    jQuery('#messages').append(html); 
+    scrollToBottom();   
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -34,14 +51,7 @@ socket.on('newLocationMessage', function(message) {
         url: message.url
     });
     jQuery('#messages').append(html);
-    // var li = jQuery('<li></li>');
-    // var a = jQuery('<a target="_blank">My current location</a>');
-    // li.text(`${message.from} ${formattedTime}: `);
-    // a.attr('href', message.url);
-    // li.append(a);
-
-    // jQuery('#messages').append(li);
-
+    scrollToBottom();
 });
 
 
